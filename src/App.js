78790ade00cfe8b1f63443f137c6a012f29c2ff0
topsecretdev0602 <allthebest1233@proton.me>
2023-01-03 21:17:11 -0500

    setup theme provider and light/dark mode context provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 const App = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider calue={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+
+          </main>
+        </div>
+      </ThemeProvider>      
+    </ColorModeContext.Provider>
   );
 }
 
