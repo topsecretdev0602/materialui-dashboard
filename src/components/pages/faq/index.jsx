8df7dc376a1481b7cd7d1fc2b7e26210095b2e89
topsecretdev0602 <allthebest1233@proton.me>
2023-01-07 19:47:53 -0500

    added table and form based pages

--- src/components/pages/faq/index.jsx
+++ src/components/pages/faq/index.jsx
@@ -1,4 +1,5 @@
-import { Box, useTheme } from '@mui/material';
+import { ExpandMore } from '@mui/icons-material';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography, useTheme } from '@mui/material';
 import React from 'react'
 import { tokens } from '../../../theme';
 import PageHeader from '../../layout/PageHeader';
@@ -6,13 +7,47 @@ import PageHeader from '../../layout/PageHeader';
 export const FAQ = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
+  const QandA = [
+    {
+      question: "First frequently asked question",
+      answer: "First Answer"
+    },
+    {
+      question: "Second frequently asked question",
+      answer: "Second Answer"
+    },
+    {
+      question: "Third frequently asked question",
+      answer: "Third Answer"
+    },
+    {
+      question: "Fourth frequently asked question",
+      answer: "Fourth Answer"
+    },
+    {
+      question: "Fifth frequently asked question",
+      answer: "Fifth Answer"
+    }
+  ];
   return (
     <Box m="20px">
         <PageHeader title="FAQ" subtitle="Frequently Asked Questions" />
-    </Box>
-      
-  )
+        {QandA && QandA.map(current => (
+          <Accordion defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              <Typography color={colors.greenAccent[500]} variant="h5">
+                {current.question}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                {current.answer}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+    </Box>    
+  );
 }
 
 export default FAQ;
